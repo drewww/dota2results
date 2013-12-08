556b87e070875ee590e5aa18e9018f1484e86c6f
Drew Harry <drew.harry@gmail.com>
2013-12-07 21:39:35 -0500

    Added the basic bits.

---
+++ dota2results.js
@@ -0,0 +1,12 @@
+var request = require('request'),
+	winston = require('winston'),
+	twit = require('twit');
+
+
+
+winston.info("dota2results STARTING");
+
+
+
+
+winston.info("dota2results ENDING");
