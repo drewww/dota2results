eea187ef7757a1681f07e2e0acfe95720d59d467
Drew Harry <drew.harry@gmail.com>
2013-12-07 23:34:48 -0500

    Ugh, tournament_only flag is apparently for JUST TI. That's annoying.

--- dota2results.js
+++ dota2results.js
@@ -12,43 +12,72 @@ winston.info("dota2results STARTING");
 
 var leagues;
 
+
+// basic arc of it:
+// on startup, load league listing.
+// check active games periodically, tracking match ids.
+// when a match id falls off the active games list, request its deets.
+// process it.
+// 
+// DITCH MATCH HISTORY BECAUSE IT LIES
+
 api.getLeagueListing(function(err, res) {
 	leagues = res.leagues;
 	winston.info("loaded " + leagues.length + " leagues");
 
-	api.getMatchHistory({tournament_games_only:1, matches_requested: 1}, function(err, res) {
-		winston.info("loading details for match " + res.matches[0].match_id);
-		// now get the tournament details
-		api.getMatchDetails(res.matches[0].match_id, function(err, match) {
+	var now = Math.floor(new Date().getTime()/1000);
+	var then = now-(60*60*24);
 
-			var teams = [];
-			_.each(["radiant", "dire"], function(name) {
-				var team = {};
-				_.each(["name", "team_id", "logo", "team_complete"], function(param) {
-					team[param] = match[name+"_"+param];
+	winston.info("requesting matches between: " + now + " and " + then);
+	api.getMatchHistory({
+		tournament_games_only:1,
+		// date_max: now,
+		// date_min: then
+	}, function(err, res) {
+
+		if(err) {
+			winston.err(err);
+			return;
+		}
+
+		if(res.num_results==0) {
+			winston.info("No matches within the last " + (now - then) + " seconds.");
+			return;
+		}
+
+		_.each(res.matches, function(match) {
+			// now get the tournament details
+			winston.info("loading details for match " + match.match_id);
+			api.getMatchDetails(match.match_id, function(err, match) {
+				var teams = [];
+				_.each(["radiant", "dire"], function(name) {
+					var team = {};
+					_.each(["name", "team_id", "logo", "team_complete"], function(param) {
+						team[param] = match[name+"_"+param];
+					});
+					team["side"] = name;
+
+					teams.push(team);
+					winston.info(team);
 				});
-				team["side"] = name;
 
-				teams.push(team);
-				winston.info(team);
+				if(!match.radiant_win) {
+					var winner = teams[1];
+					teams[1] = teams[0];
+					teams[0] = winner;
+				}
+
+				var durationString = Math.floor(match.duration/60) + ":" + match.duration%60;
+				var league = _.find(leagues, function(league) {
+					return league.leagueid==match.leagueid;
+				});
+
+
+				// var league = {name:"test"};
+
+				winston.info(teams[0].name + " DEF " + teams[1].name + "(" + durationString + ") in " + league.name);
 			});
-
-			if(!match.radiant_win) {
-				var winner = teams[1];
-				teams[1] = teams[0];
-				teams[0] = winner;
-			}
-
-			var durationString = Math.floor(match.duration/60) + ":" + match.duration%60;
-			var league = _.find(leagues, function(league) {
-				return league.leagueid==match.leagueid;
-			});
-
-
-			// var league = {name:"test"};
-
-			winston.info(teams[0].name + " DEF " + teams[1].name + "(" + durationString + ") in " + league.name);
-		});
+		})
 	});
 });
 
