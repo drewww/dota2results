653b0d41cc8d27e3bec64dff67d1a39377a41d13
Drew Harry <drew.harry@gmail.com>
2013-12-07 22:47:31 -0500

    Properly loads teams from the match info.

--- dota2results.js
+++ dota2results.js
@@ -1,6 +1,7 @@
 var request = require('request'),
 	winston = require('winston'),
 	querystring = require('querystring'),
+	_ = require('underscore')._,
 	dazzle = require('dazzle'),
 	twit = require('twit');
 
@@ -11,8 +12,27 @@ winston.info("dota2results STARTING");
 
 
 api.getMatchHistory({tournament_games_only:1, matches_requested: 1}, function(err, res) {
-	winston.info(JSON.stringify(res));
+	winston.info("loading details for match " + res.matches[0].match_id);
+	// now get the tournament details
+	api.getMatchDetails(res.matches[0].match_id, function(err, match) {
+
+		var teams = [];
+		_.each(["radiant", "dire"], function(name) {
+			var team = {};
+			_.each(["name", "team_id", "logo", "team_complete"], function(param) {
+				team[param] = match[name+"_"+param];
+			});
+			teams.push(team);
+			winston.info(team);
+		});
+
+		
+	});
 });
+
+// The desired tweet looks like this:
+// TOURNAMENT GAME N: TEAM def TEAM in TIME
+// 
 
 
 winston.info("dota2results ENDING");
