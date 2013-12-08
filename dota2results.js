49786712bf62ac438761bd4ddb242254a5399163
Drew Harry <drew.harry@gmail.com>
2013-12-07 23:11:22 -0500

    Can generate a Team A def Team B type message, including loading league data. Seems to be loading really old games first, though.

--- dota2results.js
+++ dota2results.js
@@ -10,25 +10,50 @@ var api = new dazzle(config.steam.key);
 
 winston.info("dota2results STARTING");
 
+var leagues;
 
-api.getMatchHistory({tournament_games_only:1, matches_requested: 1}, function(err, res) {
-	winston.info("loading details for match " + res.matches[0].match_id);
-	// now get the tournament details
-	api.getMatchDetails(res.matches[0].match_id, function(err, match) {
+api.getLeagueListing(function(err, res) {
+	leagues = res.leagues;
+	winston.info("loaded " + leagues.length + " leagues");
 
-		var teams = [];
-		_.each(["radiant", "dire"], function(name) {
-			var team = {};
-			_.each(["name", "team_id", "logo", "team_complete"], function(param) {
-				team[param] = match[name+"_"+param];
+	api.getMatchHistory({tournament_games_only:1, matches_requested: 1}, function(err, res) {
+		winston.info("loading details for match " + res.matches[0].match_id);
+		// now get the tournament details
+		api.getMatchDetails(res.matches[0].match_id, function(err, match) {
+
+			var teams = [];
+			_.each(["radiant", "dire"], function(name) {
+				var team = {};
+				_.each(["name", "team_id", "logo", "team_complete"], function(param) {
+					team[param] = match[name+"_"+param];
+				});
+				team["side"] = name;
+
+				teams.push(team);
+				winston.info(team);
 			});
-			teams.push(team);
-			winston.info(team);
-		});
 
-		
+			if(!match.radiant_win) {
+				var winner = teams[1];
+				teams[1] = teams[0];
+				teams[0] = winner;
+			}
+
+			var durationString = Math.floor(match.duration/60) + ":" + match.duration%60;
+			var league = _.find(leagues, function(league) {
+				return league.leagueid==match.leagueid;
+			});
+
+
+			// var league = {name:"test"};
+
+			winston.info(teams[0].name + " DEF " + teams[1].name + "(" + durationString + ") in " + league.name);
+		});
 	});
 });
+
+
+
 
 // The desired tweet looks like this:
 // TOURNAMENT GAME N: TEAM def TEAM in TIME
