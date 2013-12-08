5c2edef980c79b1ae4c8b5b346666d7eb35c65ab
Drew Harry <drew.harry@gmail.com>
2013-12-07 22:21:33 -0500

    Switched to dazzle for api calls, which is a little more convenient.

--- dota2results.js
+++ dota2results.js
@@ -1,12 +1,18 @@
 var request = require('request'),
 	winston = require('winston'),
+	querystring = require('querystring'),
+	dazzle = require('dazzle'),
 	twit = require('twit');
 
-
+var config = require('./config.json');
+var api = new dazzle(config.steam.key);
 
 winston.info("dota2results STARTING");
 
 
+api.getMatchHistory({tournament_games_only:1, matches_requested: 1}, function(err, res) {
+	winston.info(JSON.stringify(res));
+});
 
 
 winston.info("dota2results ENDING");
