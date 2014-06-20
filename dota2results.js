d7f719d6fe90613c7391716530af86790a0e282c
Drew Harry <drew.harry@gmail.com>
2014-06-20 08:44:35 -0400

    Removed match detail logging which I think was maybe too much data.

--- dota2results.js
+++ dota2results.js
@@ -668,8 +668,8 @@ exports.ResultsServer.prototype = {
 	},
 
 	processMatchDetails: function(matchDetails, matchMetadata) {
-		winston.info("matchDetails: " + JSON.stringify(matchDetails));
-		winston.info("matchMetadata: " + JSON.stringify(matchMetadata));
+		// winston.info("matchDetails: " + JSON.stringify(matchDetails));
+		// winston.info("matchMetadata: " + JSON.stringify(matchMetadata));
 		var teams = [];
 		_.each(["radiant", "dire"], function(name) {
 			var team = {};
